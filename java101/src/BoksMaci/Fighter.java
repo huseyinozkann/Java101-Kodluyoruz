c0cde6102f400af05206b62b3d63d7e2b0d136a9
Huseyin Ozkan <ozkan.hsyn1@gmail.com>
2023-02-14 14:46:33 +0300

    Java 101

---
+++ java101/src/BoksMaci/Fighter.java
@@ -0,0 +1,42 @@
+package BoksMaci;
+
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    int dodge;
+
+    Fighter(String name, int damage, int health, int weight, int dodge){
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+        if (dodge>=0 && dodge<=100){
+            this.dodge = dodge;
+        }else {
+            this.dodge = 0;
+        }
+    }
+
+    int hit(Fighter foe){
+        System.out.println("--------------------");
+        System.out.println(this.name + " => " + foe.name + " " + this.damage + " hasar vurdu.");
+
+        if (foe.dodge()){
+            System.out.println(foe.name + " gelen hasarı savurdu.");
+            return foe.health;
+        }
+        if (foe.health - this.damage < 0){
+            return 0;
+        }
+        return foe.health - this.damage;
+    }
+
+    public boolean dodge(){
+        double randomValue = Math.random() * 100;
+        return randomValue <= this.dodge;
+    }
+
+
+}
